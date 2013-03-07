e5e375b8ed4448f755fd5a8cafa003f8fe41f758
Mads Andre Lundeland <mlu@eiendomsinfo.no>
2013-03-07 12:47:46 +0100

    IE fix

--- public/javascripts/tipp.js
+++ public/javascripts/tipp.js
@@ -3,8 +3,9 @@ $(function() {
     $(".sortable").sortable({
         update: function(event, ui) {
             $('ul.sortable li').each(function(index, value) {
-                console.log(index + 1 + " : " + $(this).attr('id'))
-                $('#bet_p' + (index + 1)).val($(this).attr('id'));
+                var teamId = $(this).attr('id');
+                console.log(index + 1 + " : " + teamId)
+                $('#bet_p' + (index + 1)).val(teamId);
             });
         }
     });
